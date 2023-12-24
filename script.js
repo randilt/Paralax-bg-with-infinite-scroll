e46ec44ddf54243065f7dd87260f72a17d056b55
RandilT <randiltharusha72@gmail.com>
2023-12-24 22:38:33 +0530

    Create layers for bg animation

---
+++ script.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
+
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "layer-5.png";
+
+function animate() {
+  ctx.drawImage(backgroundLayer5, 0, 0);
+  requestAnimationFrame(animate);
+}
+animate();
